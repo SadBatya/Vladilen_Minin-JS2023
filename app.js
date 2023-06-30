1e15bb03af1d8f86891b696eaf3da17f1b9cedc3
sadbatya <sadbatya@mail.ru>
2023-06-30 22:24:33 +0200

    Todo

--- app.js
+++ app.js
@@ -1,43 +1,69 @@
 const inputElement = document.getElementById('title');
 const createBtn = document.getElementById('create');
 const listElement = document.getElementById('list');
-const notes = ['записать блок про массивы', 'рассказать теорию объектов'];
+const notes = [
+  {
+    title: 'записать блок про массивы',
+    completed: false,
+  },
+  {
+    title: 'рассказать теорию объектов',
+    completed: true,
+  },
+];
 
 function render() {
+  listElement.innerHTML = '';
+  if (notes.length === 0) {
+    listElement.innerHTML = '<p>Нет задач</p>';
+  }
   for (let i = 0; i < notes.length; i++) {
-    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]));
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i));
   }
 }
+render();
 
 createBtn.onclick = function () {
   if (inputElement.value.length === 0) {
     return;
   }
-  listElement.insertAdjacentHTML(
-    'beforeend',
-    getNoteTemplate(inputElement.value)
-  );
-
+  const newNote = {
+    title: inputElement.value,
+    completed: false,
+  };
+  notes.push(newNote);
+  render();
   inputElement.value = '';
 };
 
-function getNoteTemplate(title) {
+listElement.onclick = function (event) {
+  if (event.target.dataset.index) {
+    const index = parseInt(event.target.dataset.index);
+    const type = event.target.dataset.type;
+
+    if (type === 'toggle') {
+      notes[index].completed = !notes[index].completed;
+    } else if (type === 'remove') {
+      notes.splice(index, 1);
+    }
+
+    render();
+  }
+};
+
+function getNoteTemplate(note, index) {
   return `
   <li
             class="list-group-item d-flex justify-content-between align-items-center"
           >
-            <span>${title}</span>
+            <span class="${
+              note.completed ? 'text-decoration-line-through' : ''
+            }">${note.title}</span>
             <span>
-              <span class="btn btn-small btn-success">&check;</span>
-              <span class="btn btn-small btn-danger">&times;</span>
+              <span class="btn btn-small btn-${
+                note.completed ? 'warning' : 'succes'
+              }" data-index="${index}" data-type="toggle">&check;</span>
+              <span class="btn btn-small btn-danger" data-index ="${index}" data-type="remove">&times;</span>
             </span>
     </li>`;
 }
-
-render();
-
-const person = {
-  
-}
-console.log(typeof person)
-
