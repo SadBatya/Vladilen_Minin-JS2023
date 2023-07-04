3ed8abe0269eb004671e4222e985f0b394926e10
sadbatya <sadbatya@mail.ru>
2023-07-04 16:51:55 +0200

    fix

--- tasks/task6/task6.js
+++ tasks/task6/task6.js
@@ -1,5 +1,10 @@
 const result1 = sumNumbers(1, 2, 3, 4, 5);
-console.log(result1); // 15
-
 const result2 = sumNumbers(10, 20, 30);
+
+
+
+
+
+
+console.log(result1); // 15
 console.log(result2); // 60
